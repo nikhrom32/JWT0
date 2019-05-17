7df893cd1d26f7b05a05a976b8ec7e3b274f0266
nikhrom32 <nikhrom32@gmail.com>
2019-05-17 11:42:47 +0600

    implimenting JWT auth

--- JWT1/urls.py
+++ JWT1/urls.py
@@ -1,4 +1,4 @@
-"""JWT0 URL Configuration
+"""JWT1 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.contrib import admin
-from django.urls import path
-from core import views
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('hello/', views.HelloView.as_view(), name='hello'),
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('core.urls')),
 ]
